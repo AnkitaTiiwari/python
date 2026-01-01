805b15c21a0f8947e64277a0ea4d8ffb200a3a08
Ankita Tiwar <ankitatiwari175@gmail.com>
2026-01-02 00:39:01 +0100

    more files

--- formulaInExcel.py
+++ formulaInExcel.py
@@ -1,11 +1,15 @@
 import openpyxl
 
-wb = openpyxl.workbook()
+wb = openpyxl.Workbook()
 
 sheetName = wb.active
 
 sheetName['A1'] = 200
 sheetName['A2'] = 300
 sheetName['A3'] = '=SUM(A1:A2)'
+
+sheetName.row_dimensions[1].height = 70
+
+sheetName.column_dimensions['A'].width = 70
 
 wb.save('formulaInExcel.xlsx')
